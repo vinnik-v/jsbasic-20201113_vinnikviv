b62757f507abc8d28e0d422917520735119a6631
Vitaliy Vinnik <vinnikviv@gmail.com>
2020-11-30 22:56:00 +0300

    3-module/3-task is completed

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,5 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  return str.split('-').map((item, i) => i == 0 ? item : item.slice(0, 1).toUpperCase() + item.slice(1)).join('')
 }
