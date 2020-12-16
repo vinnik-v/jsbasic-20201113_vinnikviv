8d035a84b795d24d11c4a1e5caeb8c2c64439307
Vitaliy Vinnik <vinnikviv@gmail.com>
2020-12-16 18:44:52 +0300

    6-module/1-task is completed

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -24,11 +24,41 @@
  *          age: 25,
  *          salary: '1000',
  *          city: 'Petrozavodsk'
- *      },ы
+ *      },
  *
  * @constructor
- */
+*/
 export default class UserTable {
+
   constructor(rows) {
+    this.rows = rows;
+    this.elem = document.createElement('table')
+    this.elem.innerHTML = 
+    `<table>
+    <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>
+      <tbody>
+          ${this.rows.map(item => `<tr><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td><td><button class="delete">X</button></td></tr>`).join('')}
+      </tbody>
+    </table>`
+    this.close = this.elem.querySelectorAll('.delete');
+    for (let item of this.close) {
+      item.onclick = (event) => {
+        event.path[2].remove()
+      }
+    }
   }
+
+  get() {
+    return this.elem;
+  }
+
+
 }
